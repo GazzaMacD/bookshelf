2bff324871eb2a412dc2eda467d55d6b1a5fc200
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-09-13 07:55:15 +0200

    Add Dialog component to hold both Login and Register forms

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,20 +1,35 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 import {Logo} from 'components/logo'
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
   return (
     <div>
       <Logo height="80" width="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('Clicked Login')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('Clicked Register')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>X</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>X</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   )
 }
